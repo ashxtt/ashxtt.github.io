36075d5bf09a8252af950bcd649c5e4060064865
ashxtt <111319560+ashxtt@users.noreply.github.com>
2022-10-05 18:52:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*Menu Show*/
+/* menuShow */ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -9,40 +9,39 @@ const showMenu = (toggleId, navId) =>{
         })
     }
 }
+showMenu('nav-toggle','nav-menu')
 
-showMenu('nav-toggle', 'nav-menu')
-
-/* remove menu mobile*/
-
-const navLink = document.querySelectorAll('.nav-link')
+/* mobile menu remove*/
+const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*Scroll Section Active Link*/
+/*Scroll section active links*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
-    const scro11y = window.pageYOffset
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
 }
-sections.forEach(current =>{
-    const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id')
+window.addEventListener('scroll', scrollActive)
 
-    if(scro11Y > sectionTop && scro11Y <= sectionTop + sectionHeight){
-        document.querySelector('.nav-manu a[href*=' + sectionId + ']').classList.add('active')
-    }else{
-        document.quereySelector('.nav-menu a [href*=' + sectionId + ']').classList.remove('active')
-    }
-})
-dow.addEventListener('scroll', scrollActive)
-
-/*Scroll Reveal Animation*/
-
+/*Scroll Reveal*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
@@ -50,3 +49,8 @@ const sr = ScrollReveal({
     delay: 200,
 //     reset: true
 });
+
+sr.reveal('.home__data',{}); 
+sr.reveal('.about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.contact__input',{interval: 200}); 
